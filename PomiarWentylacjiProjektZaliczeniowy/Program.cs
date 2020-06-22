206caf29ec896f0ad57d517c09294b117c5d31ea
mimas <61653194+siagi@users.noreply.github.com>
2020-06-23 00:33:55 +0200

    Add files via upload

---
+++ PomiarWentylacjiProjektZaliczeniowy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PomiarWentylacjiProjektZaliczeniowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Witaj w aplikacji pomagającej wykonywanie pomiarów wentylacji w mieszkanich.");
+        }
+    }
+}
---
+++ VentilationLib/Addresses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VentilationLib
+{
+    public class Addresses
+    {
+        public string City { get; private set; }
+        public string HousingAssociation { get; private set; }
+        public string Street { get; private set; }
+        public string PostCode { get; private set; }
+
+    }
+
+
+}
