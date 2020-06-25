3071f08cb1edcf3b28523657817c8a4f118dbcbe
mimas <61653194+siagi@users.noreply.github.com>
2020-06-25 02:41:21 +0200

    Add files via upload

--- VentilationLib/Addresses.cs
+++ VentilationLib/Addresses.cs
@@ -9,7 +9,18 @@ namespace VentilationLib
         public string Street { get; private set; }
         public string PostCode { get; private set; }
 
-    }
+        public Addresses(string city, string housingAssociation, string street, string postCode)
+        {
+            City = city;
+            HousingAssociation = housingAssociation;
+            Street = street;
+            PostCode = postCode;
+        }
 
+        public override string ToString()
+        {
+            return $"{HousingAssociation} {Street} {PostCode} {City}";
+        }
+    }
 
 }
---
+++ VentilationLib/ListofAddresses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VentilationLib
+{
+   public class ListofAddresses
+    {
+        List<Addresses> listOfMainAddresses;
+
+        public ListofAddresses()
+        {
+            listOfMainAddresses = new List<Addresses>();
+        }
+
+        public void addNewMainAddress(string city, string housingAssociation, string street, string postCode)
+        {
+            Addresses a = new Addresses(city, housingAssociation, street, postCode);
+            listOfMainAddresses.Add(a);
+        }
+        public void readAddresses()
+        {
+            for (int i = 0; i < listOfMainAddresses.Count; i++)
+            {
+                Console.WriteLine($"{listOfMainAddresses[i]}");
+            }
+        }
+    }
+}
