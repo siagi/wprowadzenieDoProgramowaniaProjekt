077682d023105110e1c8fdd299588cceaea8e3dd
Michal Maselko <michal.maselko@gmail.com>
2020-06-25 02:53:01 +0200

    Test

--- PomiarWentylacjiProjektZaliczeniowy/Program.cs
+++ PomiarWentylacjiProjektZaliczeniowy/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Loader;
+using VentilationLib;
 
 namespace PomiarWentylacjiProjektZaliczeniowy
 {
@@ -7,6 +10,12 @@ namespace PomiarWentylacjiProjektZaliczeniowy
         static void Main(string[] args)
         {
             Console.WriteLine($"Witaj w aplikacji pomagającej wykonywanie pomiarów wentylacji w mieszkanich.");
+            Addresses myAddress = new Addresses("Krakow", "Prokocim", "Sciegiennego", "30-830");
+            ListofAddresses mainAddresses = new ListofAddresses();
+            mainAddresses.addNewMainAddress(myAddress.City, myAddress.HousingAssociation, myAddress.Street, myAddress.PostCode);
+            Console.WriteLine(myAddress);
+            mainAddresses.readAddresses();
         }
+
     }
 }
